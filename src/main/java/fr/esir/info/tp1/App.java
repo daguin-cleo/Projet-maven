4552c9b2c9864ff7cbcaa289bd1213c111d50c64
17001635 <oelc@live.fr>
2020-01-28 16:03:15 +0100

    first commit

---
+++ src/main/java/fr/esir/info/tp1/App.java
@@ -0,0 +1,23 @@
+package fr.esir.info.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+ 
+    public void codeMort() {
+    	System.out.println("Code mort");
+    }
+    
+    
+	public static void main(String[] args) {
+		int i = 1;
+        System.out.println("Hello World!");
+        if(i == 2) {
+        	System.out.println("Re mort!");
+        }
+    }
+    
+
+}
